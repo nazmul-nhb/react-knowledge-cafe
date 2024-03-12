c14efc188da1ee533dac6999e6ae96f11bb6a733
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-12 23:58:08 +0600

    fixed errors and added prop validation

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,14 @@
-const Blog = () => {
+import PropTypes from 'prop-types';
+const Blog = ({ blog }) => {
     return (
         <div>
-            
+
         </div>
     );
 };
+
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired
+}
 
 export default Blog;
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
-    const [blogs, setBlogs] = useState;
+    const [blogs, setBlogs] = useState([]);
 
     useEffect(() => {
         fetch('blogs.json')
@@ -12,7 +13,9 @@ const Blogs = () => {
 
     return (
         <div>
-
+            {
+                blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+            }
         </div>
     );
 };
