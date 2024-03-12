84772bd9bb228cc12fac077762831b21ab5f2c59
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-12 23:51:28 +0600

    created fake data and added components and fetched the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
+import Header from './Header/Header'
+import Blogs from './components/Blogs/Blogs'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold text-red-600">
-        Knowledge Cafe
-      </h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
